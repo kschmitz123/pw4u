46e88481e8b6b123cb91deaec9c856ad4fd8f36a
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-16 16:07:56 +0100

    Add setPassword and deletePassword

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 require("dotenv").config();
 const express = require("express");
-const { getPassword } = require("./lib/passwords");
+const { getPassword, setPassword, deletePassword } = require("./lib/passwords");
 const { connect } = require("./lib/database");
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get("/api/passwords/:name", async (request, response) => {
   const { name } = request.params;
-  passwordValue = await getPassword(name);
-  response.send(`The password is ${passwordValue}`);
+  try {
+    passwordValue = await getPassword(name);
+    if (!passwordValue) {
+      response.status(404).send("Could not find password.");
+    }
+    response.send(`The password is ${passwordValue}`);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("An internal server error occurred.");
+  }
 });
 
-app.listen(port, () => {
-  console.log(`App is listening at https://localhost:${port}`);
+app.post("/api/passwords/", async (request, response) => {
+  const password = request.body;
+  try {
+    await setPassword(password.name, password.value);
+    response.send(`Successfully set ${password.name}`);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("An internal server error occurred.");
+  }
+});
+app.delete("/api/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  await deletePassword(name);
+  response.send(`Password for ${name} successfully deleted`);
 });
 
 async function run() {
@@ -21,6 +42,9 @@ async function run() {
     console.log("Connecting to database...");
     await connect(process.env.MONGODB_URL, process.env.MONGODB);
     console.log("Connected to database 🎉");
+    app.listen(port, () => {
+      console.log(`App is listening at https://localhost:${port}`);
+    });
   } catch (error) {
     console.error(error);
   }
