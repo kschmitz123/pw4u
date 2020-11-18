8a6e4cbd6e874c8118372f8838a20255620f6a48
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-18 17:30:12 +0100

    Add global style

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Get from "./components/Get";
 import Delete from "./components/Delete";
 import Set from "./components/Set";
+import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components/macro";
+
+const Links = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100px;
+
+  > a {
+    text-decoration: none;
+    color: white;
+  }
+  > a:hover {
+    text-decoration: underline;
+  }
+`;
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <div className="App">
         <Switch>
           <Route exact path="/">
             <h1>Password Manager</h1>
-            <p>What do you want to do?</p>
-            <div>
+            <h3>What do you want to do?</h3>
+            <Links>
               <Link to="/get">Get passwords</Link>
-            </div>
-            <div>
-              {" "}
+
               <Link to="/delete">Delete passwords</Link>
-            </div>
-            <div>
+
               <Link to="/set">Set new passwords</Link>
-            </div>
+            </Links>
           </Route>
           <Route path="/get">
             <Get />
