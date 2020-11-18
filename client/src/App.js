c977df108f4b692ba5a1afc73f281fe7b1e5da51
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-18 10:58:33 +0100

    Add getPassword function

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { getPassword } from "./api/passwords";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [password, setPassword] = useState(null);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const newPassword = await getPassword("wifi");
+      setPassword(newPassword);
+    };
+    doFetch();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Password Manager</h1>
+      <h3>{password}</h3>
     </div>
   );
 }
